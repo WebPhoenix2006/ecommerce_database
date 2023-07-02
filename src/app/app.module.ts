7d2151c8aea0a144e64c555ab8dc2abfc795169d
Samuel Emeka <devsamuel0611@gmail.com>
2023-07-03 00:38:33 +0100

    added some products and fixed the carousel issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o/';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 500,
+    // autoplay: true,
+    // animateIn: true,
+    // animateOut: true,
+    navText: ['<i class="fa fa-angle-left d-none"></i>', '<i class="fa fa-angle-right d-none"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      1300: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+  cat_images = [
+    {tag: 'GLASSES', img_src: 'assets/img/gallery/popular1.png'},
+    {tag: 'WATCHES', img_src: 'assets/img/gallery/popular2.png'},
+    {tag: 'JACKETS', img_src: 'assets/img/gallery/popular3.png'},
+    {tag: 'CLOTHES', img_src: 'assets/img/gallery/popular4.png'}
+  ];
+  products = [
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival1.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival2.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival3.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival4.png'},
+    {title: 'Knitte Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival5.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival6.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival7.png'},
+    {title: 'Knitted Jumper', price: '$ ' + 30.00, img_src: 'assets/img/gallery/arrival8.png'},
+  ]
 }
